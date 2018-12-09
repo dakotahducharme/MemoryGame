7c4b2089a69aeda7e8d62e0587147edddbb2650c
Dakotah Ducharme <dakotah@Dakotahs-MacBook-Pro.local>
2018-12-09 15:05:55 -0800

    added reset button to reset game board, added alert to the page when cards are flipped.

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var cards = [
   {
     rank: "queen",
     suit: "hearts",
-    cardImage: "image/queen-of-hearts.png"
+    cardImage: "images/queen-of-hearts.png"
   },
   {
     rank: "queen",
@@ -26,28 +26,62 @@ var cards = [
 cardsInPlay = [];
 
 function checkForMatch() {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    console.log("You found a match!");
-  }
-  else {
-    console.log("Sorry, try again.");
+  if (cardsInPlay.length === 2) {
+    var card1 = cards[cardsInPlay[0]]
+    var card2 = cards[cardsInPlay[1]]
+    if (card1.rank === card2.rank) {
+      alert("You found a match!");
+    }
+    else {
+      alert("Sorry, try again.");
+      resetCard(cardsInPlay[0]);
+      resetCard(cardsInPlay[1]);
+    }
+    cardsInPlay = [];
   }
 }
 
-function flipCard(cardId) {
-  console.log("User flipped " + cards[cardId].rank)
-  console.log(cards[cardId].cardImage)
-  console.log(cards[cardId].suit)
-  cardsInPlay.push(cards[cardId].rank)
+function resetCard(cardId) {
+  /*
+  find dom element that has cardId
+  set source back to one of the card backs
+  */
+  // id='card-2'
+  // cardId == '2'
+  var cardElement = document.getElementById('card-' + cardId);
+  cardElement.setAttribute('src', 'images/back.png');
+
+}
+
+function resetBoard() {
+  cardsInPlay = [];
+  var board = document.getElementById('game-board');
+  while (board.hasChildNodes()) {
+    board.removeChild(board.firstChild);
+  }
+  createBoard();
+}
+
+function flipCard() {
+  var cardId = this.getAttribute('data-id')
+  cardsInPlay.push(cardId)
+  this.setAttribute('src', cards[cardId].cardImage)
   checkForMatch()
 }
 
-flipCard(3)
-flipCard(2)
 
 function createBoard() {
-  for (var i=0; i < arrayName.length; i++) {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.setAttribute('id', 'card-' + i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
 
   }
 
 }
+
+createBoard();
+document.getElementById("reset").addEventListener('click', resetBoard)
